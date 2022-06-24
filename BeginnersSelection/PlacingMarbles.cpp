b8ce4c3f249a2ed911e294cd1c353e6411819d07
nakatake <nakashima.takeo.36s@st.kyoto-u.ac.jp>
2022-06-24 17:37:35 +0900

    6/24 精選問題10選を引き続き解く

--- BeginnersSelection/PlacingMarbles.cpp
+++ BeginnersSelection/PlacingMarbles.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
+#include<string>
+// stoiを使うためには<string>をインクルードする必要がある。
+// 更にusing namespace stdを書くことでstdを省略できる。
 using namespace std;
 int main()
 {
     string str;
     cin >> str;
-    int a, b;
-    a = stoi(str);
-    
-    cout <<  << endl;
+    int a;
+    a = stoi(str.substr(0,1));
+    a += stoi(str.substr(1,1));
+    a += stoi(str.substr(2,1));
+    cout << a << endl;
     return 0;
 }
---
+++ BeginnersSelection/ShiftOnly.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    int array[200];
+    for (int i = 1; i <= a; i++)
+    {
+        cin >> array[i];
+    }
+    do
+    {
+        
+    } while ();
+    
+}
