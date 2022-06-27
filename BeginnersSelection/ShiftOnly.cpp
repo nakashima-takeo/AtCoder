a312208aa63f37dcf9d67a5372ab822f1874d367
nakatake <tonarinototorototoro8972@gmail.com>
2022-06-28 04:56:06 +0900

    6/28 更新

--- BeginnersSelection/ShiftOnly.cpp
+++ BeginnersSelection/ShiftOnly.cpp
@@ -1,17 +1,28 @@
 #include<iostream>
+#include<vector>
+#include<cmath>
 using namespace std;
 int main()
 {
     int a;
     cin >> a;
-    int array[200];
-    for (int i = 1; i <= a; i++)
+    vector<int> v(a);
+    for (int i = 0; i < a ; i++)
     {
-        cin >> array[i];
+        cin >> v[i];
     }
-    do
+    int howManyDevide = 0;
+    while (v[0] % (int)pow(2, howManyDevide + 1) == 0)
     {
-        
-    } while ();
-    
+        howManyDevide++;
+    }
+    for (int element : v)
+    {
+        while (element % (int)pow(2,howManyDevide) != 0)
+        {
+            howManyDevide--;
+        }
+    }
+    cout << howManyDevide << endl;
+    return 0;
 }
