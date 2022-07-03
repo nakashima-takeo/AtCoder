993f192cceffcc04bf00c31dc761ce0f46d06af7
nakatake <tonarinototorototoro8972@gmail.com>
2022-07-04 04:14:14 +0900

    7/4 March解いた

--- ABC/abc089/C-March.cpp
+++ ABC/abc089/C-March.cpp
@@ -1,24 +1,35 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
     int n;
     cin >> n;
-    int m,a,r,c,h;
+    vector<long long> march(5, 0);
     string s;
     char moji;
     for (int i = 1; i <= n; i++)
     {
         cin >> s;
         moji = s[0];
-        if (moji == 'M') m++;
-        else if (moji == 'A') a++;
-        else if (moji == 'R') r++;
-        else if (moji == 'C') c++;
-        else if (moji == 'H') h++;
+        if (moji == 'M') march[0]++;
+        else if (moji == 'A') march[1]++;
+        else if (moji == 'R') march[2]++;
+        else if (moji == 'C') march[3]++;
+        else if (moji == 'H') march[4]++;
     }
-    long long ans;
+    long long ans = 0;
     // 組み合わせ計算
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = i + 1; j < 4; j++)
+        {
+            for (int k = j + 1; k < 5; k++)
+            {
+                ans += march[i] * march[j] * march[k];
+            }
+        }
+    }
     cout << ans << endl;
     return 0;
 }
