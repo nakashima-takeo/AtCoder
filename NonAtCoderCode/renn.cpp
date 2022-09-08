18938c2fb9f9267f6439f3064eefe36b58ff78f5
nakatake <nakashima.takeo.36s@st.kyoto-u.ac.jp>
2022-09-08 18:36:56 +0900

    9/8 コミット

---
+++ NonAtCoderCode/renn.cpp
@@ -0,0 +1,38 @@
+#include <algorithm> // copy
+#include <iostream> // cin, cout, streamsize
+#include <iterator> // back_inserter, istream_iterator
+#include <limits> // numeric_limits
+#include <sstream> // istringstream
+#include <string> // getline, string
+#include <vector> // vector
+
+int binary_search(std::vector<int> arr, int target) {
+    int index;
+    while (arr.size() != 0)
+    {
+    }
+    return -1;
+}
+
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int main() {
+    std::vector<int> arr = get_words<int>();
+    int target;
+    std::cin >> target;
+    ignore_line();
+    int res = binary_search(arr, target);
+    std::cout << res << '\n';
+}
