5155768006b3e6f5bc3584e5b3dd3402a8779018
nakatake <tonarinototorototoro8972@gmail.com>
2022-10-16 05:46:45 +0900

    10/16 コミット

--- ABC/abc266/D-SnukePanic(1D).cpp
+++ ABC/abc266/D-SnukePanic(1D).cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 #include<vector>
 #include<tuple>
+#include<vector>
 using namespace std;
+// 貪欲法では解けない。（単調性がない）
 
-int main(){
+int main(){\jp\guardduty\
     int n;
-    long long maxVolume;
     cin >> n;
     vector<tuple<int, int, int>> sunuke(n);
     for (int i = 0; i < n; i++)
@@ -14,6 +15,44 @@ int main(){
         cin >> t >> x >> a;
         sunuke[i] = make_tuple(t,x,a);
     }
-    
+
+    vector<vector<long long>> maxVolume(n + 1, vector<long long>(5, -1));
+    int a = 0;
+    maxVolume[0][0] = 0;
+    int timeSpace = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == 1){
+            timeSpace = get<0>(sunuke[i - 1]);
+        } else {
+            timeSpace = get<0>(sunuke[i - 1]) - get<0>(sunuke[i - 2]);
+        }
+        for (int now = 0; now < 5; now++)
+        {
+            for (int previous = 0; previous < 5; previous++)
+            {
+                if (maxVolume[i - 1][previous] == -1){
+                    continue;
+                }
+                if (abs(now - previous) <= timeSpace){
+                    if (now == get<1>(sunuke[i - 1])){
+                        if (maxVolume[i - 1][previous] + get<2>(sunuke[i - 1]) > maxVolume[i][now]){
+                            maxVolume[i][now] = maxVolume[i - 1][previous] + get<2>(sunuke[i - 1]);
+                        }
+                    } else {
+                        if (maxVolume[i - 1][previous] > maxVolume[i][now]){
+                            maxVolume[i][now] = maxVolume[i - 1][previous];
+                        }
+                    }
+                }
+            }
+        }
+    }
+    long long result = 0;
+    for (auto &&hole : maxVolume[n])
+    {
+        if (hole > result) result = hole;
+    }
+    cout << result << endl;
     return 0;
 }
