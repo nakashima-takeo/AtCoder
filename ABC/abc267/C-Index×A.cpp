569a26e30e80e7b095f668a3d07aab209b4f0e6e
nakatake <tonarinototorototoro8972@gmail.com>
2022-10-18 21:42:09 +0900

    10/18 267開始

---
+++ ABC/abc267/C-Index×A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    vector<int> A(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+    
+    //処理
+}
