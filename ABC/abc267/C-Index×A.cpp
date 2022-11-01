408753f923db4f99e3164d69a9535f1f8c705c5b
nakatake <tonarinototorototoro8972@gmail.com>
2022-11-02 04:42:06 +0900

    取り敢えずコミット

--- ABC/abc267/C-Index×A.cpp
+++ ABC/abc267/C-Index×A.cpp
@@ -6,10 +6,24 @@ int main(){
     int n,m;
     cin >> n >> m;
     vector<int> A(n);
+    vector<int> S(n);
+    int sum = 0;
     for (int i = 0; i < n; i++)
     {
         cin >> A[i];
+        sum += A[i];
+        S[i] = sum;
     }
     
-    //処理
+    int max = 0;
+    for (int i = 0; i < m; i++)
+    {
+        max += A[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+    }
+    
 }
